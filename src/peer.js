e2523ede4d37f96ed5ffbebe3644bf0208895dda
Prashanth Chandra <coolshanth94@gmail.com>
2016-10-31 20:53:14 +0800

    Initial commit

---
+++ src/peer.js
@@ -0,0 +1,17 @@
+/**
+ * Created by prashanthcr on 31/10/2016.
+ */
+
+const ipfsApi = require('ipfs-api')
+const ipfs = ipfsApi() // connect to default daemon at localhost:5001
+
+
+class Peer {
+  /**
+   * Looks up hardcoded bootstrapped peer list stored in IPFS
+   * Initializes local peer store with it and persists it to disk
+   */
+  getPeers() {
+
+  }
+}
